340372d4f25cdc14a537798973dcf45c29c3a8de
mortalkomabat5 <sriramkothuru513@gmail.com>
2025-11-04 11:55:06 +0530

    Update Compare_list.py

--- Org_Automation/Compare_list.py
+++ Org_Automation/Compare_list.py
@@ -1,9 +1,14 @@
+import os 
+
 list = ["V1_abcd","V2_cejn","V300_dnmewx","V300_cenjc","V301_ceiknce"]
+#print(list[5])
+
 j=0
 list2=[]
 count=len(list2)
 list3=[]
 count=len(list)
+
 def file_read():
     if len(list)!="null":
         for i in list:
@@ -13,11 +18,14 @@ def file_read():
         #print(list2)
     else:
            print("list is empty")
-list2=['V1', 'V2', 'V300', 'V300', 'V301','V301']
-for i in range(len(list2)):   
-     for j in range(i+1,len(list2)): 
-          if(list2[i]==list2[j]):
-              print("SAME")
+
+#list2=['V1', 'V2', 'V300', 'V300', 'V301','V301']
+#for i in range(len(list2)):   
+     #for j in range(i+1,len(list2)): 
+          #if(list2[i]==list2[j]):
+              #print("SAME")
+
+file_read()
 
  
  
